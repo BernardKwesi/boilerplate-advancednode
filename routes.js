dfc3e67e5b921e580eae1979e9532b7d1b8c1713
Bernardkwesi <52609310+BernardKwesi@users.noreply.github.com>
2022-01-24 16:15:22 +0000

    added authentication with github

---
+++ routes.js
@@ -0,0 +1,90 @@
+const passport= require('passport');
+const bcrypt = require('bcrypt');
+module.exports = function (app, myDataBase) {
+
+//defining middleware to ensure user is authenticated before //accessing page
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
+
+ // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render('pug/index', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true,
+      showRegistration: true,
+      showSocialAuth: true
+    });
+  });
+
+
+app.route('/login').post(passport.authenticate('local',{ failureRedirect: '/' },function(){
+
+}),(req,res)=>{
+      res.redirect('/profile');
+});
+
+ 
+app.route("/auth/github").get(passport.authenticate('github') );
+
+app.route("/auth/github/callback").get(passport.authenticate('local', { failureRedirect: '/' }),(req,res)=>{
+      res.redirect("/profile");
+});
+
+//adding the middleware to the profile route
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+   // console.log(req.user.username)
+    res.render('pug/profile', { username: req.user.username });
+  });
+
+//unauthenticating a user 
+app.route('/logout')
+  .get((req, res) => {
+    //logout user
+    req.logout();
+    //redirect to home /
+    res.redirect('/');
+});
+
+//route to register users
+app.route('/register')
+  .post((req, res, next) => {
+    //check if username exists in db 
+    myDataBase.findOne({ username: req.body.username }, function(err, user) {
+      if (err) {
+        next(err);
+      } else if (user) { //if user is found redirect to /
+        res.redirect('/');
+      } else {
+        const hash = bcrypt.hashSync(req.body.password, 12);
+        myDataBase.insertOne({ //insert data into db 
+          username: req.body.username,
+          password: hash
+        },
+          (err, doc) => {
+            if (err) {
+              //if data isnt stored redirect to /
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },//middleware to check if user is authenticated
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+
+}
