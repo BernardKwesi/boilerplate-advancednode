77fda419adbb77b9d28d0ecaec83688f22ca88f2
Bernardkwesi <52609310+BernardKwesi@users.noreply.github.com>
2022-01-25 13:48:33 +0000

    implementing the chat feature

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ const passport= require('passport');
 const session = require('express-session');
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local');
-
-
 const routes = require("./routes.js");
 const auth = require("./auth.js");
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require("passport.socketio");
+const cookieParser = require("cookie-parser");
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 //setting pug as the template engine
 app.set('view engine', 'pug');
 //using session middleware 
@@ -45,17 +48,46 @@ myDB(async (client) => {
 routes(app, myDataBase);
 auth(app,myDataBase);
 
+
+
+
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
   let currentUsers = 0;
   io.on('connection', (socket) => {
     //increase currentUsers count by 1 when user connects
     ++currentUsers;
-    io.emit('user count', currentUsers);
+     io.emit('user', {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true
+    });
+    
+    socket.on('chat message', (message) => {
+      io.emit('chat message', { name: socket.request.user.name, message });
+    });
     console.log('A user has connected');
 
     socket.on('disconnect', () => {
   /*anything you want to do on disconnect*/
   //decrease users count by 1 when a user disconnects 
     --currentUsers;
+     io.emit('user', {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: false
+    });
+   
     io.emit('user count', currentUsers);
     console.log('A user has disconnected');
 });
@@ -85,6 +117,18 @@ app.use((req, res, next) => {
     res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
